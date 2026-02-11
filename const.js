1a8b3cf1ea9617016ae135c75b42eae695241698
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-02-11 18:04:44 +0300

    added javascript notations, and variables

---
+++ const.js
@@ -0,0 +1,6 @@
+let radius = 8
+const pi = 3.14 //some variable may need const so that they dont get tampered with
+
+let area = pi * radius * radius
+
+console.log(area)
---
+++ notation.js
@@ -0,0 +1,12 @@
+// Dot notation vs bracket notation.
+
+let myDict = {
+    cars : "Jimny",
+    year : 2015,
+    "model" : function (){
+
+    },
+}
+
+console.log(myDict.cars)//dot notation
+console.log(myDict["model"]) //bracket notation
