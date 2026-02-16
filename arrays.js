ee42937b6426d7166bfff38655260579529706bd
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-02-16 19:13:26 +0300

    added arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+//arrays are considered objects.
+
+let Colors = ['Blue', 'Green', 'Yellow'];
+Colors[3] = 'Pink';
+
+console.log(Colors[3])
---
+++ pills.js
@@ -0,0 +1,5 @@
+//taking pills
+
+let myPills = ['Blue', 'Green', 'Yellow', 'Pink', 'White', 'Orange', 'Violet'];
+
+console.log(myPills)
