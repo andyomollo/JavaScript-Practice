3515d915e082d3808a6a7a1358c2351e546306f6
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-02-24 14:42:19 +0300

    switch and control flow statements

---
+++ controlflow.js
@@ -0,0 +1,22 @@
+// Control flow statements
+
+const age = 60;
+
+if (age >= 60){
+    console.log ("You're old");
+}else{
+    console.log ("You're Young");
+}
+
+//switch statement
+const day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the week.");
+        break;
+    case "Friday":
+        console.log("End of the workweek.");
+        break;
+    default:
+        console.log("It's a regular day.");
+}
